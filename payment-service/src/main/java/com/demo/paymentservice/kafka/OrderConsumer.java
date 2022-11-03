4fe2edc6f0d489605fbfc7314c1a859bc8a05baf
buck1105 <baclxth2108041@fpt.edu.vn>
2022-11-03 15:31:40 +0700

    update payment service

---
+++ payment-service/src/main/java/com/demo/paymentservice/kafka/OrderConsumer.java
@@ -0,0 +1,22 @@
+package com.demo.paymentservice.kafka;
+
+import com.tqm.basedomains.dto.OrderEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderConsumer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderConsumer.class);
+
+    @KafkaListener(
+            topics = "${spring.kafka.topic.name}",
+            groupId = "${spring.kafka.consumer.group-id}"
+    )
+    public void consume(OrderEvent event) {
+        LOGGER.info(String.format("Order event received in payment service => %s", event));
+
+        // save the order event into the database
+    }
+}
