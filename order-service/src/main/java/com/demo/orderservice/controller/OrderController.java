c16b5870c21973d8e611880880d2a9b094052494
quanglinhh <93904360+quanglinhh@users.noreply.github.com>
2022-11-04 10:23:22 +0700

    .
    .

---
+++ order-service/src/main/java/com/demo/orderservice/controller/OrderController.java
@@ -0,0 +1,35 @@
+package com.demo.orderservice.controller;
+
+import com.demo.orderservice.kafka.OrderProducer;
+import com.tqm.basedomains.dto.Order;
+import com.tqm.basedomains.dto.OrderEvent;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1")
+public class OrderController {
+
+    private final OrderProducer orderProducer;
+
+    public OrderController(OrderProducer orderProducer) {
+        this.orderProducer = orderProducer;
+    }
+
+    @PostMapping("/order")
+    public String placeOrder(@RequestBody Order order){
+        order.setOrderId(UUID.randomUUID().toString());
+        OrderEvent orderEvent = new OrderEvent();
+        orderEvent.setStatus("PENDING");
+        orderEvent.setMessage("order status is in pending state");
+        orderEvent.setOrder(order);
+
+        orderProducer.sendMessage(orderEvent);
+        return "Oder placed successfully..";
+    }
+}
